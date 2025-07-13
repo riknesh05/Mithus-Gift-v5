446bffcddf8cf5c048dafd6397b6f79cefc7b257
riknesh05 <ariknesh05@gmail.com>
2025-07-14 01:02:41 +0530

    main bg-img {fix!}

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import bgImg from "./assets/hero_img_1.jpeg"
 
 function Hero() {
   return (
     <div>
       <div className="hero_img_main relative">
-        <img src="src/assets/hero_img_1.jpeg" alt="" className="md:h-screen w-screen " />
+        <img src={bgImg} alt="" className="md:h-screen w-screen " />
         <div className="absolute flex flex-col items-center justify-center inset-0 pt-15 bg-black/20">
           <p className=" font-extrabold text-white text-4xl ">Mithu's Gift</p>
           <p className=" font-extrabold text-white text-sm">Make your ocassion memorable with us</p>
