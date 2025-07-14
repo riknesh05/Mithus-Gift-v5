52df1ae8177808c6476b2f5c7c8fa899f98909bc
riknesh05 <ariknesh05@gmail.com>
2025-07-14 14:45:32 +0530

    new V2 Carosule {fix!}

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,104 @@ import Nav from './Nav.jsx'
 import Hero from './Hero.jsx'
 import Carasole from './Carasole.jsx'
 
+import im1 from "./assets/items/1.png";
+import im2 from "./assets/items/2.png";
+import im3 from "./assets/items/3.png";
+import im4 from "./assets/items/4.png";
+import im5 from "./assets/items/5.png";
+import im6 from "./assets/items/6.png";
+import im7 from "./assets/items/7.png";
+import im8 from "./assets/items/8.png";
+import im9 from "./assets/items/9.png";
+import im10 from "./assets/items/10.png";
+
+
+  const nArrs = [
+    {
+      Image: im1,
+      title: "Posters",
+    },
+    {
+      Image: im2,
+      title: "Thread Arts",
+    },
+    {
+      Image: im3,
+      title: "Resin Arts",
+    },
+    {
+      Image: im4,
+      title: "Key Chains",
+    },
+    {
+      Image: im5,
+      title: "Preservations",
+    },
+    {
+      Image: im6,
+      title: "Wood Arts",
+    },
+    {
+      Image: im7,
+      title: "Photo frames",
+    },
+    {
+      Image: im8,
+      title: "Gift Boxes",
+    },
+    {
+      Image: im9,
+      title: "Cards",
+    },
+    {
+      Image: im10,
+      title: "Decotations",
+    },
+  ];
+  const tDings = [
+    {
+      Image: im1,
+      title: "Art Works",
+    },
+    {
+      Image: im2,
+      title: "Thread Arts",
+    },
+    {
+      Image: im3,
+      title: "Resin Arts",
+    },
+    {
+      Image: im4,
+      title: "Key Chains",
+    },
+    {
+      Image: im5,
+      title: "Preservations",
+    },
+    {
+      Image: im6,
+      title: "Wood Arts",
+    },
+    {
+      Image: im7,
+      title: "Photo frames",
+    },
+    {
+      Image: im8,
+      title: "Gift Boxes",
+    },
+    {
+      Image: im9,
+      title: "Cards",
+    },
+    {
+      Image: im10,
+      title: "Decotations",
+    },
+  ];
+
+
 
 function App() {
 
@@ -10,8 +108,8 @@ function App() {
     <>
      <Nav /> 
      <Hero />
-     <Carasole />
-     <Carasole />
+     <Carasole data={nArrs} name={"New Arrivals"}/>
+     <Carasole data={tDings} name={"Trendings"} />
     </>
   )
 }
--- src/Carasole.jsx
+++ src/Carasole.jsx
@@ -1,21 +1,21 @@
 import React,{ Component } from "react";
-import im1 from "./assets/items/1.png";
-import im2 from "./assets/items/2.png";
-import im3 from "./assets/items/3.png";
-import im4 from "./assets/items/4.png";
-import im5 from "./assets/items/5.png";
-import im6 from "./assets/items/6.png";
-import im7 from "./assets/items/7.png";
-import im8 from "./assets/items/8.png";
-import im9 from "./assets/items/9.png";
-import im10 from "./assets/items/10.png";
+// import im1 from "./assets/items/1.png";
+// import im2 from "./assets/items/2.png";
+// import im3 from "./assets/items/3.png";
+// import im4 from "./assets/items/4.png";
+// import im5 from "./assets/items/5.png";
+// import im6 from "./assets/items/6.png";
+// import im7 from "./assets/items/7.png";
+// import im8 from "./assets/items/8.png";
+// import im9 from "./assets/items/9.png";
+// import im10 from "./assets/items/10.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./App.css";
 
 
-function Carasole() {
+function Carasole({data, name}) {
  const settings = {
  className: "center",
   centerMode: true,
@@ -57,52 +57,96 @@ function Carasole() {
     ]
   };
 
-  const data = [
-    {
-      Image: im1,
-      title: "Posters",
-    },
-    {
-      Image: im2,
-      title: "Thread Arts",
-    },
-    {
-      Image: im3,
-      title: "Resin Arts",
-    },
-    {
-      Image: im4,
-      title: "Key Chains",
-    },
-    {
-      Image: im5,
-      title: "Preservations",
-    },
-    {
-      Image: im6,
-      title: "Wood Arts",
-    },
-    {
-      Image: im7,
-      title: "Photo frames",
-    },
-    {
-      Image: im8,
-      title: "Gift Boxes",
-    },
-    {
-      Image: im9,
-      title: "Cards",
-    },
-    {
-      Image: im10,
-      title: "Decotations",
-    },
-  ];
+  // const nArrs = [
+  //   {
+  //     Image: im1,
+  //     title: "Posters",
+  //   },
+  //   {
+  //     Image: im2,
+  //     title: "Thread Arts",
+  //   },
+  //   {
+  //     Image: im3,
+  //     title: "Resin Arts",
+  //   },
+  //   {
+  //     Image: im4,
+  //     title: "Key Chains",
+  //   },
+  //   {
+  //     Image: im5,
+  //     title: "Preservations",
+  //   },
+  //   {
+  //     Image: im6,
+  //     title: "Wood Arts",
+  //   },
+  //   {
+  //     Image: im7,
+  //     title: "Photo frames",
+  //   },
+  //   {
+  //     Image: im8,
+  //     title: "Gift Boxes",
+  //   },
+  //   {
+  //     Image: im9,
+  //     title: "Cards",
+  //   },
+  //   {
+  //     Image: im10,
+  //     title: "Decotations",
+  //   },
+  // ];
+  // const tDings = [
+  //   {
+  //     Image: im1,
+  //     title: "Posters",
+  //   },
+  //   {
+  //     Image: im2,
+  //     title: "Thread Arts",
+  //   },
+  //   {
+  //     Image: im3,
+  //     title: "Resin Arts",
+  //   },
+  //   {
+  //     Image: im4,
+  //     title: "Key Chains",
+  //   },
+  //   {
+  //     Image: im5,
+  //     title: "Preservations",
+  //   },
+  //   {
+  //     Image: im6,
+  //     title: "Wood Arts",
+  //   },
+  //   {
+  //     Image: im7,
+  //     title: "Photo frames",
+  //   },
+  //   {
+  //     Image: im8,
+  //     title: "Gift Boxes",
+  //   },
+  //   {
+  //     Image: im9,
+  //     title: "Cards",
+  //   },
+  //   {
+  //     Image: im10,
+  //     title: "Decotations",
+  //   },
+  // ];
+
+  
   return (
     <>
     <div className="newArrivals p-2">
-      <h1 className="text-2xl p-2 font-bold">New Arrivals</h1>
+      <h1 className="text-2xl p-2 font-bold">{name}</h1>
     </div>
     <div className="overflow-hidden h-[300px]">
       <div className="image_container ">
