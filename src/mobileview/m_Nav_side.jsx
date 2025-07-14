b72e1a677201074534a4c9758e58b18b1d1dedb3
riknesh05 <ariknesh05@gmail.com>
2025-07-14 22:15:49 +0530

    fix-mob-view-nav-icons{fix!}

--- src/mobileview/m_Nav_side.jsx
+++ src/mobileview/m_Nav_side.jsx
@@ -1,6 +1,11 @@
 import Search from "../Components/Search";
 import Like from "../Components/like";
 import Share from "../Components/share";
+import new_arrivals_1 from '../assets/new_arrivals_1.svg';
+import best_seller_1 from '../assets/best_seller_1.svg';
+import occasions_1 from '../assets/occasions_1.svg';
+import for_him_1 from '../assets/for_him_1.svg';
+import for_her_1 from '../assets/for_her_1.svg';
 
 function side() {
   return (
@@ -11,23 +16,23 @@ function side() {
         <ul className="navList [&>*]:border-b-2 [&>*]:m-2.5 [&>*]:p-2  border-neutral-200">
           <li className="navItem ">
             <a href="#">New Arrivals</a>
-            <img src="src/assets/new_arrivals_1.svg" alt="" />
+            <img src={new_arrivals_1} alt="" />
           </li>
           <li className="navItem ">
             <a href="#">Best Sellers</a>
-            <img src="src/assets/best_seller_1.svg" alt="" />
+            <img src={best_seller_1} alt="" />
           </li>
           <li className="navItem ">
             <a href="#">Occasions</a>
-            <img src="src/assets/occasions_1.svg" alt="" />
+            <img src={occasions_1} alt="" />
           </li>
           <li className="navItem ">
             <a href="#">Gifts for Him</a>
-            <img src="src/assets/for_him_1.svg" alt="" />
+            <img src={for_him_1} alt="" />
           </li>
           <li className="navItem ">
             <a href="#">Gifts for Her</a>
-            <img src="src/assets/for_her_1.svg" alt="" />
+            <img src={for_her_1} alt="" />
           </li>
         </ul>
         <section className="flex items-center justify-around mt-4">
